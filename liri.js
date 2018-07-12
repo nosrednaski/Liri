337c92ad37eabe32964318da8fc0979459012d50
Unknown <nosrednaski@gmail.com>
2018-07-12 12:02:49 -0400

    get that healthy enviroment

---
+++ liri.js
@@ -0,0 +1,12 @@
+// Get that healthy enviro setup
+require("dotenv").config();
+const keys = require("keys-js");
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+// Twitter Functions
+var Twitter = require('twitter')
+function my-tweets() {
+
+}
